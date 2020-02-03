d94a9bac425a9bf9bbf44cc35faa6d1dc5fd243a
Benjamin Kim <benjaminykim@berkeley.edu>
2020-02-02 16:19:34 -0800

    partition search in appbar

--- lib/widgets/BaseAppBar.dart
+++ lib/widgets/BaseAppBar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 import './BaseTabBar.dart';
+import './BaseSearchBar.dart';
 
 class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -10,21 +11,7 @@ class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: APP_BAR_BG,
       leading: Icon(Icons.menu, size: 50),
       bottom: BaseTabBar(),
-      title: SizedBox(
-        height: 40,
-        child: TextFormField(
-          textAlignVertical: TextAlignVertical.center,
-          decoration: InputDecoration(
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.fromLTRB(15, 0, 5, 0),
-            border: new OutlineInputBorder(
-              borderRadius: const BorderRadius.all(const Radius.circular(20)),
-            ),
-            filled: true,
-            hintText: 'buscar...',
-          ),
-        ),
-      ),
+      title: Search(),
     );
   }
 
---
+++ lib/widgets/BaseSearchBar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (SizedBox(
+      height: 40,
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.fromLTRB(15, 0, 5, 0),
+          border: new OutlineInputBorder(
+            borderRadius: const BorderRadius.all(const Radius.circular(20)),
+          ),
+          filled: true,
+          hintText: 'buscar...',
+        ),
+      ),
+    ));
+  }
+}
