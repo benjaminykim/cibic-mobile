aa48c5bae4d5da2a96f39b93595d74f4beab2c34
Benjamin Kim <benjaminykim@berkeley.edu>
2020-02-02 22:20:50 -0800

    populate activity card information

--- lib/widgets/ActivityFeed.dart
+++ lib/widgets/ActivityFeed.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
-import './ActivityCard.dart';
+import './activity_card/ActivityCard.dart';
 
 class ActivityFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xfff2f2f2),
-      child: ListView(
-        children: <Widget>[
-          ActivityCard(),
-          ActivityCard(),
-          ActivityCard(),
-          ActivityCard(),
-          ActivityCard(),
-        ],
+      child: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: ListView(
+          children: <Widget>[
+            ActivityCard(),
+            ActivityCard(),
+            ActivityCard(),
+            ActivityCard(),
+            ActivityCard(),
+          ],
+        ),
       ),
     );
   }
