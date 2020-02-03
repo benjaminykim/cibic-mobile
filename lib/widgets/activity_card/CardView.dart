719e296f0ef0fd0c1ff8a89861dd8bbc5c316d1c
Benjamin Kim <benjaminykim@berkeley.edu>
2020-02-02 23:23:25 -0800

    update activity card view

--- lib/widgets/activity_card/CardView.dart
+++ lib/widgets/activity_card/CardView.dart
@@ -14,6 +14,56 @@ class CardView extends StatelessWidget {
           color: CARD_BACKGROUND,
           borderRadius: BorderRadius.all(Radius.circular(15)),
         ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(child: Icon(Icons.keyboard_arrow_left, size: 28)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 20 - 56,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Legalize Cannabis",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Container(
+                    width: 60,
+                    child: Center(
+                      child: Text(
+                        "Proposal",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  Text(
+                    "Cannabis should be legalized because it offers very few negative effects to society, and it is an essential medicine for a lot of people (140). ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  )
+                  // text
+                ],
+              ),
+            ),
+            SizedBox(
+              child: Icon(Icons.keyboard_arrow_right, size: 28),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/activity_card/UserMetaData.dart
+++ lib/widgets/activity_card/UserMetaData.dart
@@ -8,9 +8,9 @@ class UserMetaData extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
-        IconTag(Icon(Icons.person), 'user-name'),
-        IconTag(Icon(Icons.offline_bolt, size: 15), '1.1k'),
-        IconTag(Icon(Icons.looks), 'cabildo-name'),
+        IconTag(Icon(Icons.person, size: 20), 'user-name'),
+        IconTag(Icon(Icons.offline_bolt, size: 20), '1.1k'),
+        IconTag(Icon(Icons.looks, size: 20), 'cabildo-name'),
       ],
     );
   }
