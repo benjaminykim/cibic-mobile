1a3c331e2c44fa6cafc9ffc19c1426c290ce39cb
Benjamin Kim <benkim96@gmail.com>
2020-02-03 12:22:38 -0800

    reduce cardview widget nesting complexity

--- lib/widgets/activity_card/card_view/CardView.dart
+++ lib/widgets/activity_card/card_view/CardView.dart
@@ -16,93 +16,90 @@ class CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        SizedBox(width: 10),
-        Container(
-          width: MediaQuery.of(context).size.width - 20,
-          height: 162,
-          decoration: BoxDecoration(
-            color: CARD_BACKGROUND,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              //left
-              Icon(Icons.keyboard_arrow_left, size: 28),
-              //center
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 20 - 56,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // title
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 17, 0, 5),
-                        child: Text(
-                          title,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 20,
+        height: 162,
+        decoration: BoxDecoration(
+          color: CARD_BACKGROUND,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            //left
+            Icon(Icons.keyboard_arrow_left, size: 28),
+            //center
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 20 - 56,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // title
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 17, 0, 5),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                      // Label
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                        child: Container(
-                          width: 80,
-                          child: Center(
-                            child: Text(
-                              label,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w300,
-                              ),
+                    ),
+                    // Label
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Container(
+                        width: 80,
+                        child: Center(
+                          child: Text(
+                            label,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w300,
                             ),
                           ),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: labelColorPicker[label]),
-                              borderRadius: BorderRadius.circular(20)),
                         ),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: labelColorPicker[label]),
+                            borderRadius: BorderRadius.circular(20)),
                       ),
-                      // Content
-                      ((label == "Poll")
-                          ? (Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 20),
-                                  child: Icon(Icons.thumb_down, size: 50),
-                                ),
-                                Icon(Icons.thumb_up, size: 50)
-                              ],
-                            ))
-                          : Text(
-                              text,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w300,
+                    ),
+                    // Content
+                    ((label == "Poll")
+                        ? (Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20),
+                                child: Icon(Icons.thumb_down, size: 50),
                               ),
-                            ))
-                    ],
-                  ),
+                              Icon(Icons.thumb_up, size: 50)
+                            ],
+                          ))
+                        : Text(
+                            text,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ))
+                  ],
                 ),
               ),
-              // right
-              Icon(Icons.keyboard_arrow_right, size: 28),
-            ],
-          ),
+            ),
+            // right
+            Icon(Icons.keyboard_arrow_right, size: 28),
+          ],
         ),
-        SizedBox(width: 10),
-      ],
+      ),
     );
   }
 }
