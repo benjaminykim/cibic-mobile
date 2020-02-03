c0bba75826ec5a548264e7b8cd65ecdb3cad2229
Benjamin Kim <benkim96@gmail.com>
2020-02-03 11:31:42 -0800

    list render for activity feed

---
+++ lib/example_injection.dart
@@ -0,0 +1,77 @@
+const data_0 = const {
+  'userName': 'Sharon Gomez',
+  'cp': '1.1k',
+  'cabildoName': 'chile-cannabis',
+  'title': 'Legalize Cannabis',
+  'label': 'Proposal',
+  'text':
+      'Cannabis should be legalized because it offers very few negative effects to society, and it is an essential medicine for a lot of people',
+  'pingNum': '22.1k',
+  'commentNum': '1.1k',
+  'dateTime': '5 hours',
+};
+
+const data_1 = const {
+  'userName': 'Alonso Escalante',
+  'cp': '5.2k',
+  'cabildoName': 'Freedom Chile',
+  'title': 'Remove the President',
+  'label': 'Discuss',
+  'text':
+      'The president should be removed from office as he is unfit to lead the country. He does not represent the ideas of Chile',
+  'pingNum': '15.2k',
+  'commentNum': '800',
+  'dateTime': '1 day',
+};
+
+const data_2 = const {
+  'userName': 'Julia Maria',
+  'cp': '208',
+  'cabildoName': 'santiago-macul',
+  'title': 'Should people have guns?',
+  'label': 'Poll',
+  'text': '',
+  'pingNum': '590',
+  'commentNum': '49',
+  'dateTime': '5 days',
+};
+
+const data_3 = const {
+  'userName': 'Julio Aregano',
+  'cp': '60k',
+  'cabildoName': 'all',
+  'title': 'What do you think about free healthcare?',
+  'label': 'Discuss',
+  'text':
+      'What do you think about free healthcare in Chile? It should be paid for by taxes',
+  'pingNum': '10.2k',
+  'commentNum': '9.1k',
+  'dateTime': '6 days',
+};
+
+const data_4 = const {
+  'userName': 'Matteo Jorge',
+  'cp': '20',
+  'cabildoName': 'Freedom Chile',
+  'title': 'Police illegally arrest 2 people',
+  'label': 'Discuss',
+  'text':
+    'The police illegally detained two teenagers in Santiago at a protest last Sunday. They were peacefully protesting at Civic Square.',
+  'pingNum': '4.9k',
+  'commentNum': '42',
+  'dateTime': '6 hours',
+};
+
+const data_5 = const {
+  'userName': 'Gloria Iglesias',
+  'cp': '900',
+  'cabildoName': 'santiago-chile',
+  'title': 'Public education should be free',
+  'label': 'Poll',
+  'text': '',
+  'pingNum': '1.1k',
+  'commentNum': '271',
+  'dateTime': '2 days',
+};
+
+const FEED_DATA = const [data_3, data_1, data_2, data_4, data_0, data_5];
