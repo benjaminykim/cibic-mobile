8a59a7ef13168d89f69521dfa1a4627efcbad0d6
Benjamin Kim <benkim96@gmail.com>
2020-02-13 18:55:40 -0800

    [UPDATE] card view contents

--- lib/widgets/activity_card/card_view/CardContents.dart
+++ lib/widgets/activity_card/card_view/CardContents.dart
@@ -9,16 +9,7 @@ class CardContents extends StatelessWidget {
   final String text;
   final int mode;
   final Map<String, String> comments;
-  final Map<int, Color> labelColorPicker = {
-    ACTIVITY_PROPOSAL: LABEL_PROPOSAL_COLOR,
-    ACTIVITY_DISCUSS: LABEL_DISCUSS_COLOR,
-    ACTIVITY_POLL: LABEL_POLL_COLOR,
-  };
-  final Map<int, String> labelTextPicker = {
-    ACTIVITY_PROPOSAL: 'Proposal',
-    ACTIVITY_DISCUSS: 'Discuss',
-    ACTIVITY_POLL: 'Poll',
-  };
+
 
   CardContents(this.title, this.type, this.text, this.mode, this.comments);
 
@@ -82,47 +73,21 @@ class CardContents extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width - 20 - 56,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // title
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 17, 0, 5),
-              child: Text(
-                title,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // title
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
             ),
-            // Label
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Container(
-                width: 80,
-                child: Center(
-                  child: Text(
-                    labelTextPicker[type],
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(color: labelColorPicker[type]),
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            ),
-            // Content
-            generateCardBody()
-          ],
-        ),
+          ),
+          // Content
+          generateCardBody()
+        ],
       ),
     );
   }
