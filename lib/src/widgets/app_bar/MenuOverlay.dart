a85eb18c00efff783c421a17d9c8c848226e4496
Benjamin Kim <benkim96@gmail.com>
2020-03-25 22:47:23 -0700

    ui changes to menu overlay

--- lib/src/widgets/app_bar/MenuOverlay.dart
+++ lib/src/widgets/app_bar/MenuOverlay.dart
@@ -5,81 +5,84 @@ import '../../constants.dart';
 class MenuOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(topRight: Radius.circular(50)),
-      child: (Drawer(
-        child: Container(
-          decoration: BoxDecoration(color: APP_BAR_BG),
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: 90,
-                    child: DrawerHeader(
-                      child: Text(
-                        'cibic',
-                        style: Theme.of(context).textTheme.title,
+    return Container(
+      width: 250,
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50)),
+        child: (Drawer(
+          child: Container(
+            decoration: BoxDecoration(color: APP_BAR_BG),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 130,
+                      child: DrawerHeader(
+                        child: Text(
+                          'cibic',
+                          style: Theme.of(context).textTheme.title,
+                        ),
                       ),
                     ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: Divider(thickness: 3, color: Color(0xff828282),),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  title: Text(
+                    'Perfil',
+                    style: Theme.of(context).textTheme.body1,
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                    child: Divider(thickness: 3, color: Color(0xff828282),),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Crear cabildo',
+                    style: Theme.of(context).textTheme.body1,
                   ),
-                ],
-              ),
-              ListTile(
-                title: Text(
-                  'Profile',
-                  style: Theme.of(context).textTheme.body1,
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'Cabildos',
-                  style: Theme.of(context).textTheme.body1,
+                ListTile(
+                  title: Text(
+                    'Guardados',
+                    style: Theme.of(context).textTheme.body1,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'Saved',
-                  style: Theme.of(context).textTheme.body1,
+                ListTile(
+                  title: Text(
+                    'Configuracion',
+                    style: Theme.of(context).textTheme.body1,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'Settings',
-                  style: Theme.of(context).textTheme.body1,
+                ListTile(
+                  title: Text(
+                    'About @cibic',
+                    style: Theme.of(context).textTheme.body1,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'About',
-                  style: Theme.of(context).textTheme.body1,
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      )),
+        )),
+      ),
     );
   }
 }
