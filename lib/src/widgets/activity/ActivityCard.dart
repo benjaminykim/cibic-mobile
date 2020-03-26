465f526a62609926734acb5ef4641da1ac99a819
Benjamin Kim <benkim96@gmail.com>
2020-03-26 15:10:51 -0700

    refactor activity screen and card for modular rendering comments scroll

--- lib/src/widgets/activity/ActivityCard.dart
+++ lib/src/widgets/activity/ActivityCard.dart
@@ -9,19 +9,22 @@ import '../../models/activity_model.dart';
 class ActivityCard extends StatelessWidget {
   final ActivityModel activity;
   final Function(ActivityScreen, BuildContext) onActivityTapped;
+  final bool scrollEnabled;
 
-  ActivityCard(this.activity, this.onActivityTapped);
+  ActivityCard(this.activity, this.onActivityTapped, this.scrollEnabled);
 
   @override
   Widget build(BuildContext context) {
+    if (!this.scrollEnabled)
+      this.activity.comments = null;
+
     return GestureDetector(
       onTap: () => this.onActivityTapped(ActivityScreen(activity), context),
       child: Container(
         child: Column(
           children: <Widget>[
             UserMetaData(activity.idUser, "10.1k", activity.idCabildo),
-            CardScroll(activity.title, activity.activityType, activity.text,
-                activity.score, activity.comments),
+            CardScroll(activity.title, activity.activityType, activity.text, activity.score, activity.comments),
             CardMetaData(activity.pingNumber, activity.commentNumber,
                 activity.publishDate),
           ],
