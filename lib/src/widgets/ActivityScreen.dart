d68c70d7b2e46b2a81ff5b00662a5571c97ae9e2
Benjamin Kim <benkim96@gmail.com>
2020-03-25 20:22:11 -0700

    activity screen populates card only

---
+++ lib/src/widgets/ActivityScreen.dart
@@ -0,0 +1,30 @@
+import 'package:cibic_mobile/src/models/activity_model.dart';
+import 'package:cibic_mobile/src/widgets/activity_card/activity_components/CardMetaData.dart';
+import 'package:cibic_mobile/src/widgets/activity_card/activity_components/UserMetaData.dart';
+import 'package:cibic_mobile/src/widgets/activity_card/activity_components/card_view/CardViewScroll.dart';
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatelessWidget {
+  final ActivityModel activity;
+
+  ActivityScreen(this.activity);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            UserMetaData(activity.idUser, "10.1k", activity.idCabildo),
+            CardViewScroll(activity.title, activity.activityType, activity.text,
+                activity.score, null),
+            CardMetaData(activity.pingNumber, activity.commentNumber,
+                activity.publishDate),
+          ],
+        ),
+      ),
+    );
+  }
+}
