81541ac17a0eeb20b49ea0bd8bf39620bbf6cdb7
Benjamin Kim <benkim96@gmail.com>
2020-03-26 15:14:36 -0700

    restructure compose screen

--- lib/src/widgets/menu/AppBar.dart
+++ lib/src/widgets/menu/AppBar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../Compose.dart';
+import '../ComposeScreen.dart';
 
 class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String pageName;
