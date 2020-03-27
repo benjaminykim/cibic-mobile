8da4dbdfc75017e2f1d8e9243d47a874395d60da
Benjamin Kim <benkim96@gmail.com>
2020-03-27 00:07:29 -0700

    [UPDATE] comment reply list view dynamic render

--- lib/src/widgets/activity/components/CommentFeed.dart
+++ lib/src/widgets/activity/components/CommentFeed.dart
@@ -5,9 +5,18 @@ import 'package:cibic_mobile/src/models/comment_model.dart';
 import 'package:cibic_mobile/src/models/reply_model.dart';
 import 'package:cibic_mobile/src/widgets/activity/components/card/UserMetaData.dart';
 
-class CommentFeed extends StatelessWidget {
+class CommentFeed extends StatefulWidget {
   final List<CommentModel> comments;
+
+  CommentFeed(this.comments);
+
+  @override
+  _CommentFeedState createState() => _CommentFeedState();
+}
+
+class _CommentFeedState extends State<CommentFeed> {
   final inputCommentController = TextEditingController();
+
   final BoxShadow commentShadow = BoxShadow(
     color: Color(0xff000000),
     blurRadius: 0,
@@ -15,13 +24,13 @@ class CommentFeed extends StatelessWidget {
     offset: Offset(1, 0.1),
   );
 
-  CommentFeed(this.comments);
+  int maxCommentView = 3;
 
   Container comment(CommentModel c, BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(0, 10, 0, 2),
       width: MediaQuery.of(context).size.width - 20,
-      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
       decoration: BoxDecoration(
         color: CARD_BACKGROUND,
         borderRadius: BorderRadius.all(Radius.circular(30)),
@@ -93,87 +102,162 @@ class CommentFeed extends StatelessWidget {
             ),
           ),
           // RESPONSES
-          ...generateResponse(c.reply, context),
+          ...generateResponseFeed(c.reply, context),
         ],
       ),
     );
   }
 
-  List<Container> generateComments(BuildContext context) {
-    List<Container> commentCards = [];
-    for (var i = 0; i < comments.length; i++) {
-      commentCards.add(comment(comments[i], context));
-    }
-    return commentCards;
-  }
-
-  List<Container> generateResponse(
-      List<ReplyModel> responses, BuildContext context) {
-    if (responses != null) {
-      List<Container> responseCards = [];
-      for (int i = 0; i < responses.length; i++) {
-        responseCards.add(
+  Container reply(ReplyModel r, BuildContext c) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+      decoration: BoxDecoration(
+        color: Color(0xffcccccc),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          // REACT SYSTEM
           Container(
-            margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
-            decoration: BoxDecoration(
-              color: Color(0xffcccccc),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                // REACT SYSTEM
-                Container(
-                  padding: const EdgeInsets.fromLTRB(5, 15, 0, 10),
-                  width: 25,
-                  height: 80,
-                  child: Column(
-                    children: <Widget>[
-                      Icon(Icons.keyboard_arrow_up, size: 20),
-                      Text(
-                        responses[i].score.toString(),
-                        style: TextStyle(
-                          fontSize: 8,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Icon(Icons.keyboard_arrow_down, size: 20),
-                    ],
+            padding: const EdgeInsets.fromLTRB(5, 15, 0, 10),
+            width: 25,
+            height: 80,
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.keyboard_arrow_up, size: 20),
+                Text(
+                  r.score.toString(),
+                  style: TextStyle(
+                    fontSize: 8,
+                    color: Colors.black,
                   ),
                 ),
+                Icon(Icons.keyboard_arrow_down, size: 20),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width - 105,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // RESPONSE USER METADATA
                 Container(
-                  width: MediaQuery.of(context).size.width - 105,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      // RESPONSE USER METADATA
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 10, 30, 5),
-                        child: UserMetaData(responses[i].idUser, "1.1k", null),
-                      ),
-                      // RESPONSE TEXT CONTENT
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                        child: Text(
-                          responses[i].content,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                      // RESPONSE ICON
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 10, 5),
-                        alignment: Alignment.bottomRight,
-                        child: Icon(Icons.reply, size: 20),
-                      ),
-                    ],
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                  child: UserMetaData(r.idUser, "1.1k", null),
+                ),
+                // RESPONSE TEXT CONTENT
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                  child: Text(
+                    r.content,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
                   ),
+                ),
+                // RESPONSE ICON
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 10, 5),
+                  alignment: Alignment.bottomRight,
+                  child: Icon(Icons.reply, size: 20),
                 ),
               ],
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  List<Container> generateCommentFeed(BuildContext context) {
+    List<Container> commentCards = [];
+    for (int i = 0; i < widget.comments.length; i++) {
+      commentCards.add(comment(widget.comments[i], context));
+    }
+    return commentCards;
+  }
+
+  Container generateNewCommentInput(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 10, 20, 2),
+      width: MediaQuery.of(context).size.width - 20,
+      height: 75,
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      decoration: BoxDecoration(
+        color: CARD_BACKGROUND,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        boxShadow: [commentShadow],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Deja un comentario",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              )),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 2, 0, 0),
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            height: 33,
+            decoration: BoxDecoration(
+              color: Color(0xffcccccc),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            child: TextField(
+              controller: inputCommentController,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.send, color: Colors.black),
+                border: InputBorder.none,
+                hintText: "comenta...",
+                hintStyle: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                    fontSize: 14),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Container> generateResponseFeed(
+      List<ReplyModel> responses, BuildContext context) {
+    if (responses != null) {
+      List<Container> responseCards = [];
+      for (int i = 0;
+          i < responses.length && i < this.maxCommentView - 1;
+          i++) {
+        responseCards.add(
+          reply(responses[i], context),
+        );
+      }
+      if (responses.length > this.maxCommentView) {
+        responseCards.add(
+          // SEE MORE COMMENTS BUTTON
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 30, 5),
+            alignment: Alignment.bottomRight,
+            child: GestureDetector(
+              onTap: () => {
+                setState(() {
+                   this.maxCommentView += 2;
+                })
+              },
+              child: Text(
+                "ver mas...",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                )
+              )
+            ),
+          )
         );
       }
       return responseCards;
@@ -185,49 +269,8 @@ class CommentFeed extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 2),
-          width: MediaQuery.of(context).size.width - 20,
-          height: 75,
-          margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-          decoration: BoxDecoration(
-            color: CARD_BACKGROUND,
-            borderRadius: BorderRadius.all(Radius.circular(30)),
-            boxShadow: [commentShadow],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Deja un comentario",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black,
-                  )),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                height: 33,
-                decoration: BoxDecoration(
-                  color: Color(0xffcccccc),
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: TextField(
-                  controller: inputCommentController,
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.send, color: Colors.black),
-                    border: InputBorder.none,
-                    hintText: "comenta...",
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w200,
-                        color: Colors.black,
-                        fontSize: 14),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        ...generateComments(context),
+        generateNewCommentInput(context),
+        ...generateCommentFeed(context),
       ],
     );
   }
