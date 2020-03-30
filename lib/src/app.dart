d022cc39224ec5fb863e1bb0db121e6b800221ee
Benjamin Kim <benkim96@gmail.com>
2020-03-29 19:22:34 -0700

    user models updated, post for user creation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+
+import 'package:cibic_mobile/src/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -7,6 +12,45 @@ import 'package:cibic_mobile/src/widgets/activity/ActivityFeed.dart';
 import 'package:cibic_mobile/src/widgets/menu/AppBar.dart';
 import 'package:cibic_mobile/src/widgets/menu/BaseBar.dart';
 import 'package:cibic_mobile/src/widgets/menu/MenuOverlay.dart';
+
+Future<String> createFakeUser() async {
+  String url = 'http://10.0.2.2:3000/users';
+  Map map = {
+    'user': {
+      'username': 'ben',
+      'password': 'fakepassword',
+      'email': 'fakeEmail@gmail.com',
+      'firstName': 'ben',
+      'middleName': 'yo',
+      'lastName': 'kim',
+      'maidenName': 'hong',
+      'phone': 6266924012,
+      'rut': "123456790",
+      'cabildos': [],
+      'files': "none",
+      'followers': [],
+      'following': [],
+      'activityFeed': []
+    }
+  };
+
+  HttpClient httpClient = new HttpClient();
+  HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+  request.headers.set('content-type', 'application/json');
+  request.add(utf8.encode(json.encode(map)));
+  HttpClientResponse response = await request.close();
+  String reply;
+  if (response.statusCode == 201) {
+    final responseBody = await response.transform(utf8.decoder).join();
+    Map<String, dynamic> user = jsonDecode(responseBody);
+    reply = user['id'];
+  } else {
+    throw Exception(
+        "HTTP Response error code: " + response.statusCode.toString());
+  }
+  httpClient.close();
+  return reply;
+}
 
 class App extends StatefulWidget {
   final Client client;
@@ -24,12 +68,7 @@ class _AppState extends State<App> {
   List<dynamic> feed;
   List<Widget> _widgetOptions;
   String appBarTitle = "INICIO";
-  List<String> _feedNames = [
-    "INICIO",
-    "PUBLICO",
-    "USARIO",
-    "ESTADISTICAS"
-  ];
+  List<String> _feedNames = ["INICIO", "PUBLICO", "USARIO", "ESTADISTICAS"];
 
   void onBarButtonTapped(int index) {
     setState(() {
@@ -40,15 +79,15 @@ class _AppState extends State<App> {
 
   void onActivityTapped(ActivityScreen activityScreen, BuildContext context) {
     Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => activityScreen)
-    );
+        context, MaterialPageRoute(builder: (context) => activityScreen));
   }
 
   @override
   void initState() {
     super.initState();
-
+    createFakeUser().then((value) {
+      print(value);
+    });
     _widgetOptions = [
       ActivityFeed("home", onActivityTapped),
       Container(),
@@ -70,7 +109,8 @@ class _AppState extends State<App> {
             child: _widgetOptions.elementAt(selectedBarIndex),
           ),
           drawer: MenuOverlay(),
-          bottomNavigationBar: BaseBar(this.selectedBarIndex, this.onBarButtonTapped),
+          bottomNavigationBar:
+              BaseBar(this.selectedBarIndex, this.onBarButtonTapped),
         ),
       ),
     );
