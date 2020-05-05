fe10a5496eb44a0db62bb3ca28c5133ab1defd6f
Durosinmi Olorunfemi <dee_femifose@yahoo.com>
2020-05-04 19:14:25 -0700

    Splash ios done and setting up Icons

--- lib/src/widgets/menu/BaseBar.dart
+++ lib/src/widgets/menu/BaseBar.dart
@@ -9,31 +9,30 @@ class BaseBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 0.5)),
-            child: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.home, color: Colors.black, size: 30),
-                    title: Text("")),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.public, color: Colors.black, size: 30),
-                    title: Text("")),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person_outline,
-                        color: Colors.black, size: 30),
-                    title: Text("")),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.show_chart, color: Colors.black, size: 30),
-                    title: Text("")),
-              ],
-              type: BottomNavigationBarType.fixed,
-              currentIndex: selectedIndex,
-              onTap: onItemTapped,
-              showSelectedLabels: true,
-              showUnselectedLabels: false,
-              backgroundColor: Colors.white,
-            ),
-          );
+      decoration:
+          BoxDecoration(border: Border.all(color: Colors.grey, width: 0.5)),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.black, size: 30),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.public, color: Colors.black, size: 30),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline, color: Colors.black, size: 30),
+              title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart, color: Colors.black, size: 30),
+              title: Text("")),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
