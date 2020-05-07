93c5fa022984d603f0dbfb4d8aee405f757bcf23
Benjamin Kim <benkim96@gmail.com>
2020-05-06 18:24:05 -0700

    about finish no video

--- lib/src/widgets/menu/menu-overlay/About.dart
+++ lib/src/widgets/menu/menu-overlay/About.dart
@@ -1,184 +1,121 @@
-import 'package:cibic_mobile/src/models/cabildo_model.dart';
 import 'package:cibic_mobile/src/resources/constants.dart';
-import 'package:cibic_mobile/src/widgets/profile/CabildoProfileScreen.dart';
 import 'package:flutter/material.dart';
 
-class Configuration extends StatefulWidget {
-  final String jwt;
+class About extends StatelessWidget {
+  TextStyle style = TextStyle(
+    color: Colors.black,
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+  );
 
-  Configuration(this.jwt);
+  TextStyle contentStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 15,
+    fontWeight: FontWeight.w200,
+  );
 
-  @override
-  _ConfigurationState createState() => _ConfigurationState();
-}
+  EdgeInsets aboutPadding =  EdgeInsets.fromLTRB(30, 10, 30, 10);
 
-class _ConfigurationState extends State<Configuration> {
-  bool showNotificationSettings = false;
-  bool notificationsOn = false;
-  bool soundsOn = false;
 
-  Widget cabildoItem(CabildoModel cabildo) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) =>
-                    CabildoProfileScreen(cabildo.id, widget.jwt)));
-      },
-      child: Container(
-        height: 65,
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 50.0,
-              height: 50.0,
-              margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-              decoration: new BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
-              ),
-            ),
-            Text(
-              cabildo.name,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Divider divider = Divider(color: Colors.black, indent: 50, endIndent: 20);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: cibicTheme,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("CONFIGURACIÓN",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                )),
-            centerTitle: true,
-            titleSpacing: 0.0,
-            leading: GestureDetector(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Icon(Icons.arrow_back_ios),
-            ),
+        appBar: AppBar(
+          title: Text("ABOUT CIBIC",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              )),
+          centerTitle: true,
+          titleSpacing: 0.0,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Icon(Icons.arrow_back_ios),
           ),
-          body: Container(
-            color: APP_BACKGROUND,
-            child: ListView(
-              padding: const EdgeInsets.all(8),
-              children: <Widget>[
-                Container(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                        child: Icon(Icons.clear, size: 30),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Text(
-                            'Eliminar usuario',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+        ),
+        body: Container(
+          color: APP_BACKGROUND,
+          padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                padding: aboutPadding,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                divider,
-                Container(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                        child: Icon(Icons.lock_outline, size: 30),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Text(
-                            'Politíca de privacidad',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                divider,
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      this.showNotificationSettings =
-                          !this.showNotificationSettings;
-                    });
-                  },
-                  child: Container(
-                    height: 50,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                          child: Icon(Icons.notifications_none, size: 30),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Text(
-                              'Preferencia de notificaciones',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          ),
-                        ),
-                      ],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "¿Cómo usar cibic?",
+                      style: style,
                     ),
-                  ),
+                    SizedBox(height: 10),
+                    Text(
+                      "contacto@cibic.app",
+                      style: contentStyle,
+                    ),
+                    SizedBox(height: 10),
+                  ],
                 ),
-                (this.showNotificationSettings)
-                    ? Padding(
-                          padding: const EdgeInsets.fromLTRB(35, 0, 30, 0),
-                      child: SwitchListTile(
-                          title: const Text('Notificación en pantalla'),
-                          value: this.notificationsOn,
-                          onChanged: (bool value) {
-                            setState(() {
-                              this.notificationsOn = value;
-                            });
-                          }),
-                    )
-                    : Container(),
-                (this.showNotificationSettings)
-                    ? Padding(
-                          padding: const EdgeInsets.fromLTRB(35, 0, 30, 0),
-                      child: SwitchListTile(
-                          title: const Text('Sonidos'),
-                          value: this.soundsOn,
-                          onChanged: (bool value) {
-                            setState(() {
-                              this.soundsOn = value;
-                            });
-                          }),
-                    )
-                    : Container(),
-                divider,
-              ],
-            ),
-          )),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                padding: aboutPadding,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Acerca de CIBIC",
+                      style: style,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "CIBIC es una plataforma de uso social y de uso cívico para poder organizarnos.",
+                      style: contentStyle,
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                padding: aboutPadding,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Contacto",
+                      style: style,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "contacto@cibic.app",
+                      style: contentStyle,
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
