c2837efd4df5983b455fcc6558c8c7e6d55ae707
Benjamin Kim <benkim96@gmail.com>
2020-06-10 13:10:10 -0700

    localization to spanish enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'src/onboard/app.dart';
 
 void main() async {
@@ -6,6 +8,14 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('es', 'CL'),
+      ],
       routes: {
         '/': (context) => App(),
       }
